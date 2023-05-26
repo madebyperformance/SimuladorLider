2cf9fb914550ff870b8a0765b8bd93e133bfd9fd
madebyperformance <pedro.dantas@investsmart.com.br>
2023-05-26 14:48:31 -0300

    incluindo css no visual, coluna, made by performance e link do simulador do g10 e s10

--- lider.py
+++ lider.py
@@ -12,6 +12,18 @@ st.set_page_config(
     page_icon=im,
     layout='wide')
 
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+st.markdown('<div style="position: fixed; bottom: 0; right: 100px;"><p style="color: white;"><span style="color:black;font-size: 20px;font-family: Barlow Semibold;">MADE BY </span><span style="color:#9966FF;font-size: 20px; font-family: Barlow Semibold, sans-serif;">PERFORMANCE</span></p></div>', unsafe_allow_html=True)
+st.markdown('<div style="position: fixed; bottom: 0; right: 1420px;"><p style="color: grey; font-size: 14px;font-family: Barlow;">Criado por Pedro Dantas</p></div>', unsafe_allow_html=True)
+
+def link():
+    st.sidebar.markdown("<a href='https://madebyperformance-simulador-g10s10-simuladorg10s10-83xtig.streamlit.app/' target='_blank' style='text-decoration: none; font-family: Barlow; font-weight: bold; font-size: 22px; color: white;'>SIMULADOR TOP50</a>", unsafe_allow_html=True)
+    st.sidebar.markdown("<span style='font-family: Barlow; color: white; font-size: 14px;'>Clique acima para ser redirecionado ao Simulador do G10 e S10.</span>", unsafe_allow_html=True)
+
+link()
+
 st.title('Simulador do Partnership 2023 - Líder')
 st.caption("Use este simulador para calcular quanto de premiação você poderá receber ao final do ano. Importante frisar que a premiação é calculada em cima de valores preenchidos por você, devem ser considerados como uma aproximação.")
 
